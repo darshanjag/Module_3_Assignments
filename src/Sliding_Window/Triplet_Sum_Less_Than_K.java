df7cb1442aa8604910f7d377ad18493799a478ed
darshanjag <itsdarshanjagtap@gmail.com>
2022-10-26 13:30:16 +0530

    containers

--- src/Sliding_Window/Triplet_Sum_Less_Than_K.java
+++ src/Sliding_Window/Triplet_Sum_Less_Than_K.java
@@ -31,19 +31,15 @@ class Triplet_Sum_Less_Than_K
 
     static int countTriplets(int n, int sum, int[] arr)
     {
+        int ans =0;
         Arrays.sort(arr);
-        int ans = 0;
-
-        for (int i = 0; i < n - 2; i++)
-        {
-            int j = i + 1, k = n - 1;
-            while (j < k)
-            {
-                if (arr[i] + arr[j] + arr[k] >= sum)
+        for(int i =0; i<n-2; i++){
+            int j =i+1, k = n-1;
+            while(j<k){
+                if(arr[i]+arr[j]+arr[k]>=sum){
                     k--;
-                else
-                {
-                    ans += (k - j);
+                }else{
+                    ans+=k-j;
                     j++;
                 }
             }
