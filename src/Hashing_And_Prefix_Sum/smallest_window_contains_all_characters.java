6cf147b74aafbbe8e35d0a744a17b79e9c303add
darshanjag <itsdarshanjagtap@gmail.com>
2022-10-29 21:44:09 +0530

    smallest window contains all characters

--- src/Hashing_And_Prefix_Sum/smallest_window_contains_all_characters.java
+++ src/Hashing_And_Prefix_Sum/smallest_window_contains_all_characters.java
@@ -1,61 +1,61 @@
 package Hashing_And_Prefix_Sum;
 import java.util.*;
-public class smallest_window_contains_all_characters {
-    static String smallest_window(String s, String t) {
-        // corner case
-        if(s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) return "";
-        int minright =0;
-        int minleft= 0;
-        int count=0;
-        int min = s.length()+1;
-        boolean flag = false;
 
-        HashMap <Character,Integer> hm = new HashMap<>();
-        for(char c : t.toCharArray()){
-            hm.put(c,hm.getOrDefault(c,0)+1);
+class smallest_window_contains_all_characters {
+    static String minWindow(String s, String t) {
+
+        if (s.length() == 0 || t.length() == 0) {
+            return "-1";
         }
-        int i=0,j =0;
-        while(i<s.length()){
-            char c = s.charAt(i);
-            if(hm.containsKey(c)){
-                hm.put(c,hm.getOrDefault(c,0)-1);
-                if(hm.get(c)>=0){
-                    count++;
-                }
-            }
-            if(count==t.length()){
-                flag=true;
-                int curLen = i+1 -j;
-                if(curLen<=min){
-                    minleft= j;
-                    minright=i;
-                    min =curLen;
-                }
-            }
-            while(count==t.length()){
 
-                // shrink
-                char left = s.charAt(j);
-                if(hm.containsKey(left)){
-                    hm.put(left, hm.getOrDefault(left,0)+1);
-                    if(hm.get(left)>=1){
-                        count--;
-                    }
-                }
-                j++;
-            }
-
-
-            i++;
-
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
         }
-        return flag ? s.substring(minleft,minright+1): "";
+
+        int required = dictT.size();
+
+        int l = 0, r = 0;
+
+        int formed = 0;
+
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+
+        int[] ans = { -1, 0, 0 };
+
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+
+        return ans[0] == -1 ? "-1" : s.substring(ans[1], ans[2] + 1);
     }
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String t = sc.nextLine();
-        System.out.println(smallest_window(s,t));
+        String s = sc.next(), t = sc.next();
+
+        System.out.println(minWindow(s,t));
 
     }
 }
+
+
